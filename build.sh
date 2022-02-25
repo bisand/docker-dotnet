aa910d959ef75c8b101c59b41b61bbdf7d09a75a
André Biseth <andre@biseth.net>
2022-02-25 18:26:06 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+DOTNET_VERSION_DEFAULTVALUE=""
+DOTNET_VERSION="${1:-$DOTNET_VERSION_DEFAULTVALUE}"
+
+sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-build/. -t bisand/dotnet-build:$DOTNET_VERSION
+sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-runtime/. -t bisand/dotnet-runtime:$DOTNET_VERSION
+sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-runtime-asp/. -t bisand/dotnet-runtime-asp:$DOTNET_VERSION

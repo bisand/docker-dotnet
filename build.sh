bff2df6d038930921e1293b26e2b3e9888528c1c
André Biseth <andre@biseth.net>
2022-03-01 06:49:50 +0100

    Added demo debug project

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ DOTNET_VERSION="${1:-$DOTNET_VERSION_DEFAULTVALUE}"
 sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-build/. -t bisand/dotnet-build:$DOTNET_VERSION
 sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-runtime/. -t bisand/dotnet-runtime:$DOTNET_VERSION
 sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-runtime-asp/. -t bisand/dotnet-runtime-asp:$DOTNET_VERSION
+
+sudo docker push bisand/dotnet-build:$DOTNET_VERSION
+sudo docker push bisand/dotnet-runtime:$DOTNET_VERSION
+sudo docker push bisand/dotnet-runtime-asp:$DOTNET_VERSION

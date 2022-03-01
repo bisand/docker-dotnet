095dbd0835959124ad51e9fe09ee89f8b316c90f
André Biseth <andre@biseth.net>
2022-03-01 19:38:07 +0100

    Updated build scrpts

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-DOTNET_VERSION_DEFAULTVALUE=""
+# Current version: https://dotnetcli.azureedge.net/dotnet/Runtime/Current/latest.version
+# LTS version: https://dotnetcli.azureedge.net/dotnet/Runtime/LTS/latest.version
+
+DOTNET_VERSION_DEFAULTVALUE=$(wget -qO - https://dotnetcli.azureedge.net/dotnet/Runtime/LTS/latest.version)
 DOTNET_VERSION="${1:-$DOTNET_VERSION_DEFAULTVALUE}"
+
+echo "building images for dotnet version: $DOTNET_VERSION"
 
 sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-build/. -t bisand/dotnet-build:$DOTNET_VERSION
 sudo docker build --build-arg dotnet_version="--version ${DOTNET_VERSION}" dotnet-runtime/. -t bisand/dotnet-runtime:$DOTNET_VERSION
--- demo/Program.cs
+++ demo/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine("Hello, World! {0}", i);
+}
